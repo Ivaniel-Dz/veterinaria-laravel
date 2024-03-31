f7c6c9080a4553baa75503f471776005c236d3af
Ivaniel-Dz <pcivaniel@gmail.com>
2024-03-31 14:42:29 -0500

    add services v2

--- resources/js/Pages/Servicio.vue
+++ resources/js/Pages/Servicio.vue
@@ -1,15 +1,16 @@
 
 <template>
 
-    <h2 class="dark:text-white text-2xl font-semibold pt-8 pb-1 px-4 md:text-3xl md:px-[150px] lg:px-[250px]">Servicios</h2>
-    <section id="servicios" class="dark:text-white w-full h-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 px-4 md:px-[150px] lg:px-[250px]">
+    <h2 class="dark:text-white sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 px-4  md:px-[100px] lg:px-[100px]">Servicios</h2>
 
-        <figure v-for="servicio in servicios" :key="servicio.servicio" class="pr-4 md:px-0 rounded shadow dark:shadow-gray-700">
+    <section id="servicios" class="dark:text-white w-full h-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 px-4 md:px-[100px] lg:px-[100px]">
+
+        <figure v-for="servicio in servicios" :key="servicio.servicio" class="md:p-4 shadow dark:bg-gray-700 bg-gray-100 p-6 rounded-lg">
             <div class="flex items-center justify-center">
-                <img :src="servicio.imagen" alt="imagen" class="w-[250px] h-[200px] rounded-lg"/>
+                <img :src="servicio.imagen" alt="imagen" class="w-[250px] h-[200px] rounded-lg mb-4"/>
             </div>
-            <h2 class="text-lg md:text-xl font-semibold">{{ servicio.servicio }}</h2>
-            <p class="text-sm md:text-base">{{ servicio.descripcion }}</p>
+            <h2 class="text-lg font-medium title-font mb-2">{{ servicio.servicio }}</h2>
+            <p class="leading-relaxed text-base">{{ servicio.descripcion }}</p>
         </figure>
 
     </section>
