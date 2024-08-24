41c06f1dcd93a687c28922a0189df4eb84aa5f2b
Ivaniel-Dz <pcivaniel@gmail.com>
2024-08-24 17:18:14 -0500

    modified DataSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,17 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    {   
+        // Cantidad de Usuarios para crear
         // User::factory(10)->create();
 
+        // datos creados manual
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        Mensaje::factory(20)->create();
+        //Cantidad de Mensajes para crear
+        Mensaje::factory(5)->create();
     }
 }
